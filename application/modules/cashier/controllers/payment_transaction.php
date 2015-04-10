445d74fe14646d15491bddaf37664c499a03e124
dwi <dwi_wahyudi@yahoo.com>
2015-04-10 16:52:21 +0700

    update accounting opsifin (Workflow Transaksi - form - script)

--- application/modules/cashier/controllers/payment_transaction.php
+++ application/modules/cashier/controllers/payment_transaction.php
@@ -11,14 +11,14 @@ class Payment_transaction extends CI_Controller {
         $menu = $this->menu->set_menu();
         $this->twiggy->set('menu_navigasi', $menu);
         
-        $this->twiggy->title('OPSIFIN')->prepend('DP From Customer');;
+        $this->twiggy->title('OPSIFIN')->prepend('Payment Transaction');;
         $this->twiggy->meta('keywords', 'twiggy, twig, template, layout, codeigniter');
         $this->twiggy->meta('description', 'Twiggy is an implementation of Twig template engine for CI');
         
         // create content page fo dp supplier
-        $this->twiggy->set('BREADCRUMBS_TITLE', 'DP From Customer');
+        $this->twiggy->set('BREADCRUMBS_TITLE', 'Payment Transaction');
         $this->twiggy->set('BREADCRUMBS_MAIN_TITLE', 'Cashier');
-        $this->twiggy->set('LIST_TITLE', 'DP From Customer');
+        $this->twiggy->set('LIST_TITLE', 'Payment Transaction');
     }
     
     function index()
@@ -27,21 +27,21 @@ class Payment_transaction extends CI_Controller {
         // create content page fo dp supplier
         $content = $this->twiggy->template('breadcrumbs')->render();
         //$content .= $this->twiggy->template('form/filter_dp_supplier')->render();        
-        $content .= $this->twiggy->template('list/dp_customer')->render();
+        $content .= $this->twiggy->template('list/payment_transaction')->render();
         // end        
         $this->twiggy->set('content_page', $content);
         
-        $this->twiggy->set('FORM_NAME', 'form_dp_customer');
+        $this->twiggy->set('FORM_NAME', 'form_payment_transaction');
         $this->twiggy->set('FORM_EDIT_IDKEY', 'data-edit-id');
         $this->twiggy->set('FORM_DELETE_IDKEY', 'data-delete-id');        
-        $this->twiggy->set('FORM_IDKEY', 'full.dc_no');
-        $this->twiggy->set('FORM_LINK', site_url('cashier/dp_customer/delete'));
+        $this->twiggy->set('FORM_IDKEY', 'full.payment_no');
+        $this->twiggy->set('FORM_LINK', site_url('cashier/payment_transaction/delete'));
         
         $button_crud = $this->twiggy->template('button/btn_edit')->render();         
         $button_crud .= $this->twiggy->template('button/btn_del')->render();
         $this->twiggy->set('BUTTON_CRUD', $button_crud);
         
-        $script_page = $this->twiggy->template('script/dp_customer')->render();         
+        $script_page = $this->twiggy->template('script/payment_transaction')->render();         
         //$script_page .= $this->twiggy->template('script/script_all')->render();         
         
         $this->twiggy->set('SCRIPTS', $script_page);
@@ -53,22 +53,22 @@ class Payment_transaction extends CI_Controller {
     {
         $data = array();
         if (!empty($id)){
-            $this->load->model('modeldpcustomer');
+            $this->load->model('modelpayment');
             $data = $this->modeldpcustomer->getdataid($id);
             $this->twiggy->set('edit', $data); 
         };
         
         // create content page fo dp supplier
         //$content = $this->twiggy->template('breadcrumbs')->render();
-        $content = $this->twiggy->template('form/form_dp_customer')->render();        
+        $content = $this->twiggy->template('form/form_payment_transaction')->render();        
         // end        
         $this->twiggy->set('content_page', $content);
         
         $this->twiggy->set('FORM_NAME', 'form_dp_customer');
         $this->twiggy->set('FORM_EDIT_IDKEY', 'data-edit-id');
         $this->twiggy->set('FORM_DELETE_IDKEY', 'data-delete-id');        
-        $this->twiggy->set('FORM_IDKEY', 'full.id_dp_customer');
-        $this->twiggy->set('FORM_LINK', site_url('cashier/dp_customer/delete'));
+        $this->twiggy->set('FORM_IDKEY', 'full.payment_detail_id');
+        $this->twiggy->set('FORM_LINK', site_url('cashier/payment_transaction/delete'));
         
         $button_crud = $this->twiggy->template('button/btn_edit')->render();         
         $button_crud .= $this->twiggy->template('button/btn_del')->render();
@@ -83,7 +83,7 @@ class Payment_transaction extends CI_Controller {
         $this->twiggy->set('window_page', $window_page);
         
         
-        $script_page = $this->twiggy->template('script/form_dp_customer')->render();         
+        $script_page = $this->twiggy->template('script/form_payment_transaction')->render();         
         $script_page .= $this->twiggy->template('script/script_dept')->render();
         $script_page .= $this->twiggy->template('script/script_customer')->render();
         $script_page .= $this->twiggy->template('script/script_currency')->render();
@@ -98,24 +98,24 @@ class Payment_transaction extends CI_Controller {
         
     function save()
     {
-        $this->load->model('modeldpcustomer');
+        $this->load->model('modelpayment');
         $params = (object) $this->input->post();   
         $valid = $this->modeldpcustomer->save($params);
         
         
         if (empty($valid))
-            $this->owner->alert("Please complete the form", site_url('cashier/dp_customer/form'));
+            $this->owner->alert("Please complete the form", site_url('cashier/payment_transaction/form'));
 	else
-            redirect(site_url('cashier/dp_customer/'), "refresh");
+            redirect(site_url('cashier/payment_transaction/index'), "refresh");
     }   
     
     public function delete($id)
 	{	
-                $this->load->model('modeldpcustomer');
+                $this->load->model('modelpayment');
 		$valid = false;
 		$valid = $this->modeldpcustomer->delete($id);
 		
 		if ($valid)
-			redirect(site_url("cashier/dp_customer/index"), "refresh");	
+			redirect(site_url("cashier/payment_transaction/index"), "refresh");	
 	}
 }
